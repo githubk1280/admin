1a55693304a53d3d999a81df975752e4730e40b1
terry198807 <2414547918@qq.com>
2014-11-05 23:34:22 +0800

    sql

---
+++ src/main/resources/sql/employee_role.sql
@@ -0,0 +1,8 @@
+/*
+-- Query: SELECT * FROM test.employee_role
+LIMIT 0, 1000
+
+-- Date: 2014-11-05 23:33
+*/
+INSERT INTO `employee_role` (`EMPLOYEE_ROLE_ID`,`EMPLOYEE_ROLE`,`DESCRIPTION`) VALUES (1,'E','employee');
+INSERT INTO `employee_role` (`EMPLOYEE_ROLE_ID`,`EMPLOYEE_ROLE`,`DESCRIPTION`) VALUES (2,'M','manager');
---
+++ src/main/resources/sql/project_emp.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: SELECT * FROM test.project_employee
+LIMIT 0, 1000
+
+-- Date: 2014-11-05 23:31
+*/
+INSERT INTO `project_employee` (`PROJECT_ID`,`EMPLOYEE_ID`) VALUES (1,2);
+INSERT INTO `project_employee` (`PROJECT_ID`,`EMPLOYEE_ID`) VALUES (2,2);
+INSERT INTO `project_employee` (`PROJECT_ID`,`EMPLOYEE_ID`) VALUES (3,2);
+INSERT INTO `project_employee` (`PROJECT_ID`,`EMPLOYEE_ID`) VALUES (4,2);
+INSERT INTO `project_employee` (`PROJECT_ID`,`EMPLOYEE_ID`) VALUES (4,1);
