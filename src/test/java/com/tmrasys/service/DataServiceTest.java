92f35ee82537d34241ae2e58f1eca2adfbd56093
terry198807 <2414547918@qq.com>
2014-11-06 21:59:40 +0800

    add outSource service and dao

--- src/test/java/com/tmrasys/service/DataServiceTest.java
+++ src/test/java/com/tmrasys/service/DataServiceTest.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.tmrasys.base.AbstractBaseTestConfig;
 import com.tmrasys.domain.Employee;
+import com.tmrasys.domain.OutSource;
 import com.tmrasys.service.employee.EmployeeService;
+import com.tmrasys.service.outSource.OutSourceService;
 import com.tmrasys.service.project.ProjectService;
 
 public class DataServiceTest extends AbstractBaseTestConfig {
@@ -14,7 +16,11 @@ public class DataServiceTest extends AbstractBaseTestConfig {
 	ProjectService projectDataService;
 	@Autowired
 	EmployeeService employeeDataService;
+	@Autowired
+	OutSourceService outSourceService;
+	
 	private Employee employee;
+
 	@Test
 	public void testLoadProjectById() {
 		System.out.println(projectDataService.loadProjectById(1));
@@ -32,6 +38,12 @@ public class DataServiceTest extends AbstractBaseTestConfig {
 	public void testgetProjectByEmployee() {
 		employee = employeeDataService.getEmployeeByName("terry");
 		System.out.println(projectDataService.loadProjectsByEmployee(employee).size());
+	}
+	
+	@Test
+	public void testgetOutSourceByProjectId() {
+		OutSource os = outSourceService.getOutSourceByProjectId(2);
+		System.out.println(os);
 	}
 
 }
