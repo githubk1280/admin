682c931c2207d1f00a4fde3f90236ac4be0b0bef
terry198807 <2414547918@qq.com>
2014-11-09 11:56:13 +0800

    add sql

---
+++ src/main/resources/sql/projectProgress.sql
@@ -0,0 +1,7 @@
+/*
+-- Query: SELECT * FROM test.project_progress
+LIMIT 0, 1000
+
+-- Date: 2014-11-09 11:55
+*/
+INSERT INTO `project_progress` (`PROJECT_PROGRESS_ID`,`EMPLOYEE_NAME`,`PROGRESS_CONTENT`,`PROGRESS_DATE`,`PROJECT_ID`) VALUES (1,'terry','预付款已打','2014-11-11',1);
