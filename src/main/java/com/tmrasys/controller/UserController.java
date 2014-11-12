f4ef5b5591de124e7744486685c09976c80c4d70
james.jiang <james.jiang@dianping.com>
2014-11-13 00:25:24 +0800

    update

--- src/main/java/com/tmrasys/controller/UserController.java
+++ src/main/java/com/tmrasys/controller/UserController.java
@@ -6,7 +6,6 @@ import javax.servlet.http.HttpSession;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
