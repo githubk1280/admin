64c1e95c44e993a5b1a0130b83b6957d9ade1eb1
james.jiang <james.jiang@dianping.com>
2015-01-15 00:42:57 +0800

    del legacy

--- src/main/resources/sql/employee_role.sql
+++
@@ -1,8 +0,0 @@
-/*
--- Query: SELECT * FROM test.employee_role
-LIMIT 0, 1000
-
--- Date: 2014-11-05 23:33
-*/
-INSERT INTO `employee_role` (`EMPLOYEE_ROLE_ID`,`EMPLOYEE_ROLE`,`DESCRIPTION`) VALUES (1,'E','employee');
-INSERT INTO `employee_role` (`EMPLOYEE_ROLE_ID`,`EMPLOYEE_ROLE`,`DESCRIPTION`) VALUES (2,'M','manager');
--- src/main/resources/sql/privilege.sql
+++
@@ -1,8 +0,0 @@
-/*
--- Query: SELECT * FROM test.privilege
-LIMIT 0, 1000
-
--- Date: 2014-11-05 23:33
-*/
-INSERT INTO `privilege` (`PRIVILEGE_ID`,`PRIVILEGE_CODE`,`DESCRIPTION`) VALUES (1,'E','employee');
-INSERT INTO `privilege` (`PRIVILEGE_ID`,`PRIVILEGE_CODE`,`DESCRIPTION`) VALUES (2,'M','manager');
